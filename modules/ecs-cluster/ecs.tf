5e6e555ea8ab2da7e8e6acf1076d39eb433b9a20
jys94 <115811321+jys94@users.noreply.github.com>
2023-01-05 09:05:22 +0900

    Update ecs.tf

--- modules/ecs-cluster/ecs.tf
+++ modules/ecs-cluster/ecs.tf
@@ -1,44 +1,41 @@
 #
 # ECS ami
 #
-
 data "aws_ami" "ecs" {
   most_recent = true
-
   filter {
     name   = "name"
-    values = ["amzn-ami-*-amazon-ecs-optimized"]
+    values = ["amzn2-ami-ecs-hvm-*-x86_64-ebs"]
   }
-
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
   owners = ["591542846629"] # AWS
 }
 
 #
 # ECS cluster
 #
-
 resource "aws_ecs_cluster" "cluster" {
-  name = var.cluster_name
+  name = "${var.CLUSTER_NAME}"
 }
 
 #
 # launchconfig
 #
 resource "aws_launch_configuration" "cluster" {
-  name_prefix          = "ecs-${var.cluster_name}-launchconfig"
-  image_id             = data.aws_ami.ecs.id
-  instance_type        = var.instance_type
-  key_name             = var.ssh_key_name
-  iam_instance_profile = aws_iam_instance_profile.cluster-ec2-role.id
-  security_groups      = [aws_security_group.cluster.id]
-  user_data            = templatefile("${path.module}/templates/ecs_init.tpl", {
+  name_prefix          = "ecs-${var.CLUSTER_NAME}-launchconfig"
+  image_id             = "${data.aws_ami.ecs.id}"
+  instance_type        = "${var.INSTANCE_TYPE}"
+  key_name             = "${var.SSH_KEY_NAME}"
+  iam_instance_profile = "${aws_iam_instance_profile.cluster-ec2-role.id}"
+  security_groups      = ["${aws_security_group.cluster.id}"]
+
+  user_data = templatefile("${path.module}/templates/ecs_init.tpl", {
     cluster_name = var.cluster_name
   })
+
   lifecycle {
     create_before_destroy = true
   }
@@ -48,18 +45,16 @@ resource "aws_launch_configuration" "cluster" {
 # autoscaling
 #
 resource "aws_autoscaling_group" "cluster" {
-  name                 = "ecs-${var.cluster_name}-autoscaling"
-  vpc_zone_identifier  = split(",", var.vpc_subnets)
-  launch_configuration = aws_launch_configuration.cluster.name
-  termination_policies = split(",", var.ecs_termination_policies)
-  min_size             = var.ecs_minsize
-  max_size             = var.ecs_maxsize
-  desired_capacity     = var.ecs_desired_capacity
-
+  name                 = "ecs-${var.CLUSTER_NAME}-autoscaling"
+  vpc_zone_identifier  = ["${split(",", var.VPC_SUBNETS)}"]
+  launch_configuration = "${aws_launch_configuration.cluster.name}"
+  termination_policies = ["${split(",", var.ECS_TERMINATION_POLICIES)}"]
+  min_size             = "${var.ECS_MINSIZE}"
+  max_size             = "${var.ECS_MAXSIZE}"
+  desired_capacity     = "${var.ECS_DESIRED_CAPACITY}"
   tag {
     key                 = "Name"
-    value               = "${var.cluster_name}-ecs"
+    value               = "${var.CLUSTER_NAME}-ecs"
     propagate_at_launch = true
   }
 }
-
